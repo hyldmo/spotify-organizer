6f694350fa8a6904593edae64010f24caee1232a
eivindh <eivind_h@outlook.com>
2017-10-05 21:42:56 +0200

    Added filters

--- src/utils/filterPlaylists.ts
+++ src/utils/filterPlaylists.ts
@@ -6,9 +6,9 @@ import { compareByKey } from './sort'
 export function applyPlaylistsFilters (playlists: Playlist[], filters: Filters['playlists'], user: User): Playlist[] {
 	const filteredPlaylists = playlists
 		.slice()
-		.filter(p =>
-			(_.includes(p.name, filters.text) || _.includes(p.owner.display_name, filters.text))
-		)
+		.filter(p => _.includes(p.name, filters.text) || _.includes(p.owner.display_name, filters.text))
+		.filter(p => filters.ownedOnly ? p.owner.id === user.name : true)
+		.filter(p => filters.hideEmpty ? p.tracks.total > 0 : true)
 
 	return filters.order !== null
 		? filteredPlaylists.sort((a, b) => compareByKey(a, b, filters.order.key, !filters.order.asc))
