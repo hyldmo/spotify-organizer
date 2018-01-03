9b980532088d6f459cf01337b47f03a7e51d592b
eivindh <eivind_h@outlook.com>
2018-01-03 17:05:05 +0100

    Added different comparison types

--- __tests__/utils/deduplicate.test.ts
+++ __tests__/utils/deduplicate.test.ts
@@ -1,9 +1,13 @@
 import { Track } from '../../src/types'
-import { deduplicate } from '../../src/utils'
+import { CompareType, deduplicate } from '../../src/utils'
 
-function makeTrack (name: string, artist: string | string[], album: string, duration = 180000, id = ''): Partial<Track> {
+function makeArtist (name: string): SpotifyApi.ArtistObjectSimplified {
+	return { id: name, name, href: '', type: 'artist', uri: '', external_urls: { spotify: '' } }
+}
+
+function makeTrack (name: string, artists: string | string[], album: string, duration = 180000, id?: string): Partial<Track> {
 	return {
-		id,
+		id: id || (Math.random() * 9999999999).toString(),
 		name,
 		album: {
 			album_type: '',
@@ -15,9 +19,9 @@ function makeTrack (name: string, artist: string | string[], album: string, dura
 			type: 'album',
 			uri: ''
 		},
-		artists: [
-
-		],
+		artists: artists instanceof Array
+			? artists.map(a => makeArtist(a))
+			: [ makeArtist(artists) ],
 		duration_ms: duration,
 		meta: {
 			added_at: new Date().toISOString(),
@@ -45,3 +49,39 @@ it('removes tracks with same id', () => {
 
 	expect(deduplicatedTracks.length).toBe(1)
 })
+
+it('removes tracks with same name when compareMode is Name', () => {
+	const tracks: Array<Partial<Track>> = [
+		makeTrack('song', 'artist', 'album', 0),
+		makeTrack('song', 'artist', 'album2', 1000)
+	]
+
+	const deduplicatedTracks = deduplicate(tracks as Track[], CompareType.Name)
+
+	expect(deduplicatedTracks.length).toBe(1)
+})
+
+it('removes tracks with same name and album name when compareMode is NameAndAlbum', () => {
+	const tracks: Array<Partial<Track>> = [
+		makeTrack('song', 'artist', 'album', 0),
+		makeTrack('song', 'artist', 'album', 1000),
+		makeTrack('song', 'artist', 'album', 2000)
+	]
+
+	const deduplicatedTracks = deduplicate(tracks as Track[], CompareType.NameAndAlbum)
+
+	expect(deduplicatedTracks.length).toBe(1)
+})
+
+it('removes tracks with same name and duration when compareMode is NameAndDuration', () => {
+	const tracks: Array<Partial<Track>> = [
+		makeTrack('song', 'artist', 'album', 4400),
+		makeTrack('song', 'artist', 'album2', 4000),
+		makeTrack('song', 'artist', 'album3', 4000)
+	]
+
+	const deduplicatedTracks = deduplicate(tracks as Track[], CompareType.NameAndDuration)
+
+	expect(deduplicatedTracks.length).toBe(1)
+})
+
--- src/utils/deduplicate.ts
+++ src/utils/deduplicate.ts
@@ -1,8 +1,38 @@
 import * as _ from 'lodash'
 import { Track } from '../types'
 
-export function deduplicate (tracks: Track[]): Track[] {
-	return _.uniqWith(tracks, (a, b) => {
-		return a.id === b.id
+export enum CompareType {
+	Id,
+	Name,
+	NameAndAlbum,
+	NameAndDuration
+}
+
+export function deduplicate (tracks: Track[], compareType = CompareType.Id): Track[] {
+	const simpleTracks = tracks.map(track => ({
+		id: track.id,
+		name: track.name,
+		artists: track.artists.map(artist => artist.name).sort(),
+		album: track.album.name,
+		duration: Math.round(track.duration_ms / 1000)
+	}))
+
+	const filteredTracks =  _.uniqWith(simpleTracks, (a, b) => {
+		if (a.id === b.id)
+			return true
+		if (!_.isEqual(a.artists, b.artists))
+			return false
+		switch (compareType) {
+			case CompareType.Name:
+				return a.name === b.name
+			case CompareType.NameAndAlbum:
+				return a.name === b.name && a.album === b.album
+			case CompareType.NameAndDuration:
+				return a.name === b.name && a.duration === b.duration
+			default:
+				return false
+		}
 	})
+
+	return filteredTracks.map(track => tracks.find(t => t.id === track.id) as Track)
 }
