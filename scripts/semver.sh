b135734382ce9975b12b0a27394a9634e7c6181c
Eivind <eivindh3@gmail.com>
2022-02-07 22:09:44 +0100

    ci: improved stability

--- scripts/semver.sh
+++ scripts/semver.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-set -e
+
 # Get version number
-yarn release --dry-run --no-ci | grep -oP 'next release version is \K[0-9]+\.[0-9]+\.[0-9]+' > .version
+yarn release --dry-run --no-ci -p > release.log
+cat release.log
+grep -oP 'next release version is \K[0-9]+\.[0-9]+\.[0-9]+' release.log > .version
 
 VERSION=$(cat .version)
 
 if [ -s .version ]; then
 	echo "Next version: $VERSION"
+	cat .version | awk '{print "PACKAGE_VERSION="$1}' >> .env
+	echo "::set-output name=VERSION::$VERSION"
 else
 	echo "No new version. Canceling deploy."
 fi
-cat .version | awk '{print "PACKAGE_VERSION="$1}' >> .env
-
-echo "::set-output name=VERSION::$VERSION"
