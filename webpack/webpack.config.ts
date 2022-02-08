c8ec204281fb36ac27b385899c973444855053c1
Eivind <eivindh3@gmail.com>
2022-02-08 23:30:22 +0100

    fix: source map and service worker registration

--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -54,7 +54,7 @@ const config: Configuration = {
 		}),
 		new SourceMapDevToolPlugin({
 			filename: '[file].map',
-			append: false // Let browser automatically look
+			publicPath: '/'
 		})
 	],
 
